290f19e799661517605e15ad71b1c216fd59a863
50-Course <erioluwaadeodu@my.uopeople.edu>
2022-03-14 19:48:48 +0100

    Initial Commit

---
+++ DEV_README.md
@@ -0,0 +1,1 @@
+# The Development Guide for Developers/Engineers
---
+++ android/app/src/main/kotlin/com/example/afrokitchen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.afrokitchen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
